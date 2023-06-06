03b3ecea3c35524584b6c20c6eb1d9f4b1de5fa2
abc4571998 <abc4571998@hanyang.ac.kr>
2023-06-06 23:39:26 +0900

    Feat: SubMenubar

---
+++ src/components/Menubar/SubMenubar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+const SubMenubar = ({ subMenuList }) => {
+  const [select, setSelect] = useState(subMenuList[0]);
+
+  return (
+    <SubMenu>
+      {subMenuList.map((item, idx) => {
+        return (
+          <SubMenuItem
+            className={item === select ? "select" : ""}
+            onClick={() => {
+              setSelect(item);
+            }}
+            key={idx}
+          >
+            {item}
+          </SubMenuItem>
+        );
+      })}
+    </SubMenu>
+  );
+};
+
+export default SubMenubar;
+
+const SubMenu = styled.div`
+  height: 6vh;
+  min-height: 3rem;
+  border-bottom: 2px solid #f4f4f4;
+  padding: 0 40px;
+  display: flex;
+  .select {
+    color: #3b71b9;
+    font-weight: 700;
+    border-bottom: 2px solid #3b71b9;
+  }
+`;
+
+const SubMenuItem = styled.div`
+  height: 100%;
+  font-size: 1.5rem;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #23354d;
+  font-weight: 500;
+  cursor: pointer;
+`;
