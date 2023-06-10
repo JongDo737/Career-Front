8dfbba885c1c173754743c5f20b32aad80f2117d
abc4571998 <abc4571998@hanyang.ac.kr>
2023-06-10 17:03:44 +0900

    Feat: consultList 목록형 (consultListShort) 추가

--- src/components/List/ConsultList.jsx
+++ src/components/List/ConsultList.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import ConsultItem from "./ConsultItem";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 const ConsultList = (props) => {
   const [moveIndex, setMoveIndex] = useState(0);
   const onMoveLeft = () => {
@@ -36,6 +37,50 @@ ConsultList.defaultProps = {
 };
 export default ConsultList;
 
+export const ConsultListShort = (props) => {
+  return (
+    <ShortWrapper>
+      <Table>
+        <tr className="frame">
+          <th className="num">No</th>
+          <th className="name">학생 이름</th>
+          <th className="date">날짜</th>
+          <th className="time">시간</th>
+          <th className="detail">더보기</th>
+        </tr>
+        {props.consultList.map((item, i) => {
+          return (
+            <tr>
+              <td>{i}</td>
+              <td>{item.name}</td>
+              <td>
+                {item.startTime.getFullYear()}.{item.startTime.getMonth()}.
+                {item.startTime.getDate()}
+              </td>
+              <td>
+                {item.startTime.getHours()}:{item.startTime.getMinutes()} ~
+                {item.endTime.getHours()}:{item.endTime.getMinutes()}
+              </td>
+              <td>
+                <FontAwesomeIcon icon={faAngleDown} />
+              </td>
+            </tr>
+          );
+        })}
+      </Table>
+      <Select>
+        {/* show 방식은 api 호출로 pagination 이랑 같이 진행 -> 나중에 수정 */}
+        <option value="5">show 5</option>
+        <option value="10">show 10</option>
+      </Select>
+    </ShortWrapper>
+  );
+};
+
+ConsultListShort.defaultProps = {
+  color: "white",
+};
+
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
@@ -60,4 +105,40 @@ const Div = styled.div`
     right: 0;
     top: 0;
   }
+`;
+
+const Table = styled.table`
+  width: 100%;
+  font-size: 1.3rem;
+  border-collapse: collapse;
+  .frame {
+    height: 3rem;
+    color: white;
+    background-color: #23354d;
+  }
+  td,
+  th {
+    text-align: center;
+    border: 1px solid #23354d;
+  }
+  td {
+    height: 2rem;
+  }
+`;
+
+const Select = styled.select`
+  width: 6rem;
+  font-size: 1rem;
+  background-color: white;
+  color: #23354d;
+  padding: 5px;
+  border: 1px solid #23354d;
+`;
+
+const ShortWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 20px;
 `;
